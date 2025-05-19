5ef703da9093846443a53adedfdb9e0ec680ddea
zhangjie <beimian.zj@raycloud.com>
2025-05-19 13:44:02 +0800

    10. Deploy your NFTs on the testnet

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+import {console} from "forge-std/console.sol";
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 contract MintBasicNft is Script {
     string public constant TOKENURI = "https://ipfs.io/ipns/k51qzi5uqu5dht1yivji8p2g9dmzth5akn2os2ams04hjrmgzplthtjdqpuiow";
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
-        mintNftOnContract(mostRecentlyDeployed);
+        //  Vm.DirEntry[] memory entries = vm.readDir("./broadcast", 3);
+        // console.log("entries", entries);
+        // address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid, "broadcast");
+        
+        // NOTE: Set a environment variable BASIC_NFT_ADDRESS
+        address contractAddress = vm.envAddress("BASIC_NFT_ADDRESS");        
+        mintNftOnContract(contractAddress);
+        // mintNftOnContract(mostRecentlyDeployed);
     }
 
     function mintNftOnContract(address contractAddress) public {
